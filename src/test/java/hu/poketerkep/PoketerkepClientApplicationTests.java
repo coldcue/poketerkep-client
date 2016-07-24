574641e441cc03dd2af5133f79b0e8f689f04a40
Andras Szell <coldcue@gmail.com>
2016-07-24 13:41:29 +0200

    first commit

---
+++ src/test/java/hu/poketerkep/PoketerkepClientApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.poketerkep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PoketerkepClientApplication.class)
+public class PoketerkepClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
