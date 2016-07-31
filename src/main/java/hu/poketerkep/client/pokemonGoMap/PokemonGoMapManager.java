95ea8315b9cd08f560658a3b5871b08709f0aeb3
Andras Szell <coldcue@gmail.com>
2016-07-31 03:15:21 +0200

    Threads reduced to 4

--- src/main/java/hu/poketerkep/client/pokemonGoMap/PokemonGoMapManager.java
+++ src/main/java/hu/poketerkep/client/pokemonGoMap/PokemonGoMapManager.java
@@ -40,7 +40,7 @@ public class PokemonGoMapManager implements SmartLifecycle {
                 "-st", Integer.toString(conf.getSteps()),
                 "-k", conf.getGoogleMapsKey(),
                 "-l", conf.getLocation(),
-                "-t", Integer.toString(6));
+                "-t", Integer.toString(4));
 
         processBuilder.directory(DIR);
         processBuilder.redirectErrorStream(true);
